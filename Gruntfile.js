d301660ba877f2ede9004fdff33386c08d7cbee0
twozh <xiaogen-zhang@163.com>
2014-08-16 23:24:41 +0800

    add custom-bootstrap.less

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,39 @@ module.exports = function(grunt) {
 	/* project configuration */
 	grunt.initConfig({
 		watch: {
-			options: {
-				livereload: true,
+			less_file:{
+				files: ['public/stylesheets/*.less'],
+				tasks: ['less'],
 			},
+
 			static_file:{
-				files: ['**/*'],
-			},
+				files: ['public/**/*.js', 'public/**/*.css', 'views/**/*.jade'],
+				options: {
+					livereload: true,
+				},
+			}
 		},
+
+		less: {
+			compileCore: {
+				options: {
+					strictMath: true,
+					sourceMap: true,
+					outputSourceFiles: true,
+					sourceMapURL: 'custom-bootstrap.css.map',
+					sourceMapFilename: 'public/stylesheets/custom-bootstrap.css.map'
+				},
+
+				files: {
+					'public/stylesheets/custom-bootstrap.css': 'public/stylesheets/custom-bootstrap.less'
+				}
+			}
+		}
 	});
 
 	/* load plugin */
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
 
 	/* Default task(s). */
 	grunt.registerTask('default', ['watch']);
