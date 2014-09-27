c5a8f0b498e3d45804bd5e72e81c8e5d86ee43ae
twozh <xiaogen-zhang@163.com>
2014-09-26 23:35:23 -0700

    everyday

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 		watch: {
 			less_file:{
 				files: ['public/stylesheets/*.less'],
-				tasks: ['less'],
+				tasks: ['less', 'cssmin'],
 			},
 
 			static_file:{
@@ -30,12 +30,26 @@ module.exports = function(grunt) {
 					'public/stylesheets/main.css': 'public/stylesheets/main.less'
 				}
 			}
+		},
+
+		cssmin: {
+			options: {
+				compatibility: 'ie8',
+				keepSpecialComments: '*',
+				noAdvanced: true
+			},
+			core: {
+				files: {
+					'public/stylesheets/custom-bootstrap.min.css': 'public/stylesheets/custom-bootstrap.css'
+				}
+			},
 		}
 	});
 
 	/* load plugin */
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	/* Default task(s). */
 	grunt.registerTask('default', ['watch']);
